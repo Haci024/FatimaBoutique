74efd3d7998d9a0495f34e350bfe25d6b35d8278
Samedusa4real <samadnm@code.edu.az>
2024-04-23 11:21:01 -0800

    filemanager and pagination added to proj

---
+++ Bussiness/Helpers/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Helpers
+{
+    public static class FileManager
+    {
+        public static string Save(string rootPath, string folder, IFormFile file)
+        {
+            string newFileName = Guid.NewGuid().ToString() + file.FileName;
+            string path = Path.Combine(rootPath, folder, newFileName);
+            using (FileStream str = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(str);
+            }
+
+            return newFileName;
+        }
+
+        public static bool Delete(string rootPath, string folder, string fileName)
+        {
+            string path = Path.Combine(rootPath, folder, fileName);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                return true;
+            }
+            return false;
+        }
+    }
+}
